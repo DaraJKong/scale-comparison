95fa2771007b368e5fb0c3caab7586a1eeb4d36c
DaraJKong <daraetkong@gmail.com>
2026-01-02 20:40:09 -0500

    Display things on canvas with camera & scale controls

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::num::NonZeroUsize;
 
 use lexical::{WriteFloatOptions, WriteFloatOptionsBuilder};
+use xilem::vello::kurbo::Affine;
 
 pub mod math;
 pub mod units;
@@ -14,4 +15,20 @@ const WF_OPTIONS: WriteFloatOptions = WriteFloatOptionsBuilder::new()
 #[inline]
 fn float_to_string(value: f64) -> String {
     lexical::to_string_with_options::<_, { FORMAT }>(value, &WF_OPTIONS)
+}
+
+pub fn ignore_x(trans: Affine) -> Affine {
+    let mut c = trans.as_coeffs();
+    c[0] = 1.;
+    c[2] = 0.;
+    c[4] = 0.;
+    Affine::new(c)
+}
+
+pub fn ignore_y(trans: Affine) -> Affine {
+    let mut c = trans.as_coeffs();
+    c[1] = 0.;
+    c[3] = 1.;
+    c[5] = 0.;
+    Affine::new(c)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 use xilem::{
     EventLoop, WidgetView, WindowOptions, Xilem,
-    core::{Edit, Read, lens},
-    view::{flex_col, label},
+    core::Edit,
+    masonry::properties::types::AsUnit,
+    palette::css,
+    style::Style,
+    vello::{
+        kurbo::{Affine, Circle, Line, Point, Rect, Stroke},
+        peniko::Fill,
+    },
+    view::{MainAxisAlignment, canvas, flex_col, flex_row, label, sized_box, slider, zstack},
     winit::error::EventLoopError,
 };
 
-use scale_comparison::units::TimeScale;
+use scale_comparison::{ignore_x, ignore_y, math::ENumber, units::TimeScale};
+
+const BAR_WIDTH: f64 = 50.0;
+const BAR_HALF: f64 = BAR_WIDTH / 2.;
+const BAR_GAP: f64 = 25.0;
 
 struct Thing {
     name: String,
@@ -19,40 +30,82 @@ impl Thing {
             value: value.into(),
         }
     }
-
-    fn view(&self) -> impl WidgetView<Read<Self>> + use<> {
-        label(format!("{}: {}", self.name, self.value))
-    }
 }
 
 #[derive(Default)]
 struct AppState {
+    camera: Affine,
+    scale: f64,
     things: Vec<Thing>,
 }
 
-fn app_logic(state: &mut AppState) -> impl WidgetView<Edit<AppState>> + use<> {
-    let things = state
-        .things
-        .iter()
-        .enumerate()
-        .map(|(i, _)| {
-            lens(Thing::view, move |state: &mut AppState, ()| {
-                state.things.get(i).unwrap()
-            })
-        })
-        .collect::<Vec<_>>();
-    flex_col(things)
+impl AppState {
+    fn view(&mut self) -> impl WidgetView<Edit<Self>> + use<> {
+        zstack((
+            canvas(|state: &mut Self, scene, size| {
+                let half_size = size.to_vec2() / 2.;
+                let world_view = Affine::FLIP_Y.then_translate(half_size);
+                let camera_trans = state.camera.inverse();
+                let camera_view = world_view * camera_trans;
+
+                for (i, thing) in state.things.iter().enumerate() {
+                    let x = -(BAR_WIDTH + BAR_GAP) * i as f64 - BAR_HALF;
+                    let value = (thing.value.inner() / ENumber::from_exp(state.scale))
+                        .limit_collapse(1000.);
+                    let rect = Rect::from_origin_size((x, 0.), (BAR_WIDTH, value));
+                    scene.fill(Fill::NonZero, camera_view, css::WHITE, None, &rect);
+                }
+
+                // axes rendering
+                let x_line = Line::new((-half_size.x, 0.), (half_size.x, 0.));
+                let y_line = Line::new((0., -half_size.y), (0., half_size.y));
+                let origin_dot = Circle::new(Point::ZERO, 2.);
+                scene.stroke(
+                    &Stroke::new(0.5),
+                    world_view * ignore_x(camera_trans),
+                    css::RED,
+                    None,
+                    &x_line,
+                );
+                scene.stroke(
+                    &Stroke::new(0.5),
+                    world_view * ignore_y(camera_trans),
+                    css::BLUE,
+                    None,
+                    &y_line,
+                );
+                scene.fill(Fill::NonZero, camera_view, css::GREEN, None, &origin_dot);
+            }),
+            sized_box(
+                flex_col(flex_row((
+                    sized_box(label(format!("{:.3}", self.scale))).width(60.px()),
+                    slider(-24., 18., self.scale, |state: &mut Self, value| {
+                        state.scale = value;
+                    }),
+                )))
+                .main_axis_alignment(MainAxisAlignment::End),
+            )
+            .expand()
+            .padding(5.),
+        ))
+    }
 }
 
 fn main() -> Result<(), EventLoopError> {
     let app_state = AppState {
+        camera: Affine::IDENTITY,
+        scale: 1.,
         things: vec![
             Thing::new("Hydrogen-7 half-life", (2.3, -23)),
             Thing::new("Time for sunlight to reach earth", 8. * 60. + 20.),
             Thing::new("Week", (6.048, 5)),
-            Thing::new("Sun's lifespan", (3.15576, 17)),
+            Thing::new("Sun's lifespan", (3.1556952, 17)),
         ],
     };
-    Xilem::new_simple(app_state, app_logic, WindowOptions::new("Scale Comparison"))
-        .run_in(EventLoop::with_user_event())
+    Xilem::new_simple(
+        app_state,
+        AppState::view,
+        WindowOptions::new("Scale Comparison"),
+    )
+    .run_in(EventLoop::with_user_event())
 }
